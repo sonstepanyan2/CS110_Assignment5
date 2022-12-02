3688e21eb849b2d150a791e4e36fdcc221d125b0
sonstepanyan2 <117843080+sonstepanyan2@users.noreply.github.com>
2022-12-02 22:01:35 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const utils = require('./utils.js');
+
+let mat1 = [
+    [1,2,3],
+    [2,3,4],
+    [3,3,3]
+
+]
+
+let mat2 = [
+    [2,3,4],
+    [4,5,6],
+    [5,5,5]
+]
+let text = "   This is my assignment";
+
+console.log(utils.matrixProduct(mat1,mat2));
+console.log(utils.matrixSum(mat1,mat2));
+console.log(utils.sumofrows(mat1));
+console.log(utils.countWords(text));
